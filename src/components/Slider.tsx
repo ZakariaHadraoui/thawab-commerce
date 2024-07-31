fc20254c9e752ac261d14ba14aaa80f06d1c2e17
zakaria hadraoui <zikoxo05@gmail.com>
2024-07-31 11:21:46 +0100

    thawabshopping

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -80,10 +80,10 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+      <div className="absolute m-auto left-56 bottom-8 flex gap-4 mr-24">
         {slides.map((slide, index) => (
           <div
-            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+            className={`w-3 h-3    rounded-full ring-1 ring-zak cursor-pointer flex items-center justify-center ${
               current === index ? "scale-150" : ""
             }`}
             key={slide.id}
