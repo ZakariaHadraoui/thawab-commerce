c6d52a41bcad42afe378f85cd41b4a186975688c
zakaria hadraoui <zikoxo05@gmail.com>
2024-08-01 13:39:02 +0100

    swipers

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import  { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -17,101 +15,77 @@ import { Pagination } from 'swiper/modules';
 function CategoryList() {
   return (
 	<div className="px-4 overflow-x-scroll scrollbar-hide">
-	<div className="flex gap-4 md:gap-8">
 	<Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-		<SwiperSlide>
-			<Link
-		  href={`/`}
-		  className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4"
-		  
-		>
+	  slidesPerView={1} 
+	  spaceBetween={20}
+	  pagination={{ clickable: true }}
+	  modules={[Pagination]}
+	  breakpoints={{
+		
+		640: { 
+		},
+		768: { 
+		  slidesPerView: 2,
+		},
+		1024: { 
+		  slidesPerView: 2,
+		},
+		1280: { 
+		  slidesPerView: 2,
+		},
+	  }}
+	  className="mySwiper"
+	>
+	  <SwiperSlide>
+		<Link href={`/`} className="flex-shrink-0 w-full sm:w-full lg:w-1/4 xl:w-1/4">
 		  <div className="relative bg-slate-100 w-full h-96">
 			<Image
 			  src={"/woman.png"}
-			  alt=""
+			  alt="Shoes"
 			  fill
 			  sizes="20vw"
 			  className="object-cover"
 			/>
 		  </div>
-		  <h1 className="mt-8 font-light text-xl tracking-wide">
+		  <h1 className="mt-8 font-light text-xl tracking-wide ml-4">
 			shoes
 		  </h1>
 		</Link>
-		</SwiperSlide>
-		<SwiperSlide>
-			<Link
-		  href={`/`}
-		  className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4"
-		  
-		>
+	  </SwiperSlide>
+	  <SwiperSlide>
+		<Link href={`/`} className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4">
 		  <div className="relative bg-slate-100 w-full h-96">
 			<Image
 			  src={"/woman.png"}
-			  alt=""
+			  alt="Shoes"
 			  fill
 			  sizes="20vw"
 			  className="object-cover"
 			/>
 		  </div>
-		  <h1 className="mt-8 font-light text-xl tracking-wide">
+		  <h1 className="mt-8 font-light text-xl tracking-wide ml-4">
 			shoes
 		  </h1>
 		</Link>
-		</SwiperSlide>
-		<SwiperSlide>
-			<Link
-		  href={`/`}
-		  className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4"
-		  
-		>
+	  </SwiperSlide>
+	  <SwiperSlide>
+		<Link href={`/`} className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4">
 		  <div className="relative bg-slate-100 w-full h-96">
 			<Image
 			  src={"/woman.png"}
-			  alt=""
+			  alt="Shoes"
 			  fill
 			  sizes="20vw"
 			  className="object-cover"
 			/>
 		  </div>
-		  <h1 className="mt-8 font-light text-xl tracking-wide">
+		  <h1 className="mt-8 font-light text-xl tracking-wide ml-4">
 			shoes
 		  </h1>
 		</Link>
-		</SwiperSlide>
-		<SwiperSlide>
-			<Link
-		  href={`/`}
-		  className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/4"
-		  
-		>
-		  <div className="relative bg-slate-100 w-full h-96">
-			<Image
-			  src={"/woman.png"}
-			  alt=""
-			  fill
-			  sizes="20vw"
-			  className="object-cover"
-			/>
-		  </div>
-		  <h1 className="mt-8 font-light text-xl tracking-wide">
-			shoes
-		  </h1>
-		</Link>
-		</SwiperSlide>
+	  </SwiperSlide>
+	  {/* Add more SwiperSlide components as needed */}
 	</Swiper>
-		
-		
-	
-	</div>
   </div>
   )
 }
